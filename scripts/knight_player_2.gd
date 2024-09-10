6ecdaa3b7aea29cb1771a41515cda2278ab13a02
Keane Albright <kkalbrig25@wabash.edu>
2024-09-10 15:10:05 -0400

    commit number 1

--- scripts/knight_player_2.gd
+++ scripts/knight_player_2.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction: Vector2 = Vector2(0,1)
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+func _ready() -> void:
+	animation_tree.set(starting_direction)
 
 func _physics_process(delta: float) -> void:
 	var input_direction = Vector2(
@@ -15,3 +20,8 @@ func _physics_process(delta: float) -> void:
 	
 	#move and slide function uses velocity of character body to move character on map
 	move_and_slide()
+	
+	func update_animation_parameters(move_input : Vector2):
+		if(move_input != Vector2.ZERO):
+			animation_tree.set("parameters/walk/blend_position", move_input)
+			animation_tree.set("parameters/Idle/blend_position", move_input)
