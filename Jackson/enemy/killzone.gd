cb86731ec03b258d8b6b72f2172187496df448d0
jcleeper65 <115950100+jcleeper65@users.noreply.github.com>
2024-11-21 13:45:52 -0500

    Update killzone.gd

--- Jackson/enemy/killzone.gd
+++ Jackson/enemy/killzone.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
+@onready var inventory = preload("res://inventory/playerinv.tres")
 
 @onready var timer = $Timer
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
+                for i in range(0,12):
+                	inventory.remove_at_index(i)
+			inventory.removeItemAtIndex(i)
 		print("you died")
 		Engine.time_scale=0.5
 		body.get_node("CollisionShape2D").queue_free()
